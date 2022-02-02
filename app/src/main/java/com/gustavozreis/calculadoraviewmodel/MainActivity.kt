ca6038af077c376982b11af6ec9ed628fc83689b
gustavozreis <gzreis@gmail.com>
2022-02-02 19:58:09 -0300

    Data binding pra enviar o texto do botao como parametro

--- app/src/main/java/com/gustavozreis/calculadoraviewmodel/MainActivity.kt
+++ app/src/main/java/com/gustavozreis/calculadoraviewmodel/MainActivity.kt
@@ -2,27 +2,41 @@ package com.gustavozreis.calculadoraviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.gustavozreis.calculadoraviewmodel.model.CalculatorViewModel
 import com.gustavozreis.calculadoraviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var binding: ActivityMainBinding? = null
+    private var displayDigits: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //instanciar view binding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this@MainActivity
 
         // instanciar ViewModel
         val viewModel: CalculatorViewModel by viewModels()
 
-        val displayDigits = binding.tvDisplayDigits
-        displayDigits.text = viewModel.displayDigitsList[0]
+        displayDigits = binding.tvDisplayDigits
+        displayDigits?.text = viewModel.displayDigitsList[0]
+
+
 
     }
+
+    fun appendDigit(view: TextView) {
+        displayDigits?.append(view.text)
+        Toast.makeText(this, "funcao chamada", Toast.LENGTH_LONG).show()
+    }
+
+
 
 
 
--- app/src/main/java/com/gustavozreis/calculadoraviewmodel/model/CalculatorViewModel.kt
+++ app/src/main/java/com/gustavozreis/calculadoraviewmodel/model/CalculatorViewModel.kt
@@ -1,13 +1,14 @@
 package com.gustavozreis.calculadoraviewmodel.model
 
 import androidx.lifecycle.ViewModel
+import com.gustavozreis.calculadoraviewmodel.MainActivity
 
 class CalculatorViewModel : ViewModel() {
 
     val displayDigitsList: MutableList<String> = mutableListOf("0123")
 
-    fun appendDigit(digit: String){
-        displayDigitsList.add(digit)
+    fun appendDigit(): Unit{
+
     }
 
     fun displayDigits(displayDigitsList: MutableList<String>): String {
